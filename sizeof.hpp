5ecb6595e39b59a1f3f920bb5537b964e23172bf
Mountagha <muntaghaba@gmail.com>
2026-03-02 15:54:20 -0500

    ex1 still.

--- sizeof.hpp
+++ sizeof.hpp
@@ -1,15 +1,17 @@
 
+#include <type_traits>
+
 namespace meta {
     template<typename T> struct size_of {
         using U = std::remove_cv_t<std::remove_reference_t<T>>;
-        static_assert(!std::is_void<U>, "meta::size_of operator does not support void type.");
+        static_assert(!std::is_void<U>::value, "meta::size_of operator does not support void type.");
         static constexpr std::size_t value = sizeof(U);
     };
 
-    template<typename T, std::size_t N> 
-    struct size_equals: std::bool_constant<(size_of<T>::value == N)> {};
+    template<typename T, std::size_t N>
+    struct size_equals : std::integral_constant<bool, (size_of<T>::value == N)> {};
 
     template<typename T, std::size_t N> 
-    inline constexpr bool size_equals_v = size_equals_v<T, N>::value;
+    constexpr bool size_equals_v = size_equals<T, N>::value;
 
 }
